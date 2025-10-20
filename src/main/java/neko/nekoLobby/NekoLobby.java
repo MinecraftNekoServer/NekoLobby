f733972d1e392190c7ea10ed18b1d3d366ce3133
Nyanyagulugulu <qianxun114514@gmail.com>
2025-10-20 15:19:52 +0800

    出生点锁定

--- src/main/java/neko/nekoLobby/NekoLobby.java
+++ src/main/java/neko/nekoLobby/NekoLobby.java
@@ -8,6 +8,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public final class NekoLobby extends JavaPlugin implements Listener {
 
@@ -36,5 +42,52 @@ public final class NekoLobby extends JavaPlugin implements Listener {
         
         // 取消其他所有实体的生成
         event.setCancelled(true);
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("spawn")) {
+            if (args.length > 0 && args[0].equalsIgnoreCase("set")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(ChatColor.RED + "只有玩家可以设置出生点!");
+                    return true;
+                }
+                
+                Player player = (Player) sender;
+                Location loc = player.getLocation();
+                
+                // 保存位置和朝向到配置文件
+                getConfig().set("spawn.world", loc.getWorld().getName());
+                getConfig().set("spawn.x", loc.getX());
+                getConfig().set("spawn.y", loc.getY());
+                getConfig().set("spawn.z", loc.getZ());
+                getConfig().set("spawn.yaw", loc.getYaw());
+                getConfig().set("spawn.pitch", loc.getPitch());
+                saveConfig();
+                
+                player.sendMessage(ChatColor.GREEN + "出生点已设置!");
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        FileConfiguration config = getConfig();
+        
+        // 检查是否设置了出生点
+        if (config.contains("spawn.world")) {
+            String worldName = config.getString("spawn.world");
+            double x = config.getDouble("spawn.x");
+            double y = config.getDouble("spawn.y");
+            double z = config.getDouble("spawn.z");
+            float yaw = (float) config.getDouble("spawn.yaw");
+            float pitch = (float) config.getDouble("spawn.pitch");
+            
+            Location spawnLocation = new Location(getServer().getWorld(worldName), x, y, z, yaw, pitch);
+            player.teleport(spawnLocation);
+        }
     }
 }
